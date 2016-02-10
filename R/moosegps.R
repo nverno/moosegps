ea71becd5f20d45d1bf313a4ede7be51ed293e2c
Noah Peart <noah.v.peart@gmail.com>
2016-02-09 19:29:58 -0500

    fixed titles

--- R/moosegps.R
+++ R/moosegps.R
@@ -1,8 +1,9 @@
-##' Packaged data for Moosilauke plot locations.  This includes location information
-##' for permanent plots, transects, and contours (segments), as well as some other
-##' assorted locations referred to as 'OTHER'.
+##' Packaged data for Moosilauke plot locations
 ##'
-##' Cleaning scripts and raw-data are included in directories 'clean' and 'raw'.
+##' This includes location information
+##' for permanent plots, transects, and contours (segments), as well as some other
+##' assorted locations referred to as 'OTHER'. Cleaning scripts and raw-data are 
+##' included in directories 'clean' and 'raw'.
 ##' 
 ##' @name moosegps
 ##' @import data.table
